abfc616a3bdbd869eed77c9dbbaa2017f05e1abe
Ruben Boon <Ruben95bo@gmail.com>
2023-11-25 19:50:55 +0100

    changes cdn to true

--- src/api/sanityApi.ts
+++ src/api/sanityApi.ts
@@ -4,7 +4,7 @@ const config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2023-02-08",
-  useCdn: false,
+  useCdn: true,
 };
 export const client = createClient(config);
 
